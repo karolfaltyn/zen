dba750603c549f4e0f74dfb105e6614f46adf687
Karol Faltyn <karolfaltyn03@gmail.com>
2024-04-15 14:22:41 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./assets/style.css";
+
+export const App = () => {
+  return (
+    <>
+      <h1 className="m-12 text-3xl text-red-400">Vite + React</h1>
+      <p className="p-4">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+};
