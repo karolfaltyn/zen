234eedb6e620923d39d0a768048d1163c06a6579
Karol Faltyn <karolfaltyn03@gmail.com>
2024-05-14 12:21:15 +0200

    introduced hash routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./assets/style.css";
-// import { HomePage } from "./pages/HomePage";
-// import { PomodoroPage } from "./pages/PomodoroPage";
+import { HomePage } from "./pages/HomePage";
+import { PomodoroPage } from "./pages/PomodoroPage";
 import { TimerPage } from "./pages/TimerPage";
 
 export const App = () => {
   return (
-    <>
-      {/* <HomePage /> */}
-      {/* <PomodoroPage /> */}
-      <TimerPage />
-    </>
+    <HashRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="pomodoro" element={<PomodoroPage />} />
+        <Route path="timer" element={<TimerPage />} />
+      </Routes>
+    </HashRouter>
   );
 };
