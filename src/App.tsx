9991bcc1e60fcb91eaa64aa5c08cb79e58f9f919
Karol Faltyn <karolfaltyn03@gmail.com>
2024-05-21 11:34:28 +0200

    added dark mode to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { toggleTheme } from "./utils/toggleTheme";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import "./assets/style.css";
 import { Home } from "./pages/Home";
@@ -5,6 +7,16 @@ import { PomodoroPage } from "./pages/PomodoroPage";
 import { TimerPage } from "./pages/TimerPage";
 
 export const App = () => {
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("isDarkMode");
+
+    if (isDarkMode) {
+      document.documentElement.classList.toggle("dark", JSON.parse(isDarkMode));
+      document.body.classList.toggle("bg-[#181a1b]", JSON.parse(isDarkMode));
+      toggleTheme();
+    }
+  }, []);
+
   return (
     <HashRouter>
       <Routes>
